f3000ac38b77c5323ad1b621ce5c87a270e93d79
Izaias Neto <izaiasneto.dev@gmail.com>
2022-11-18 14:41:30 -0300

    Format.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { UsersModule } from './users/users.module';
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(typeOrmConfig),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
