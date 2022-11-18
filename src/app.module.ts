6d297752cfe071f43201ef4cf9f548eb772b5aae
Izaias Neto <izaiasneto.dev@gmail.com>
2022-11-18 12:12:11 -0300

    Adding modules to app.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(typeOrmConfig)],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
