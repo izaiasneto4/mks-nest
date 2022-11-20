0f04d35f846e0885eb587bd4a204fd9a7e4181c0
Izaias Neto <izaiasneto.dev@gmail.com>
2022-11-20 02:19:27 -0300

    Bring typeorm config to app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app/app.controller';
+import { User } from './users/user.entity';
 
 @Module({
   imports: [
-    AuthModule,
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(typeOrmConfig),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [User],
+      synchronize: true,
+    }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [],
