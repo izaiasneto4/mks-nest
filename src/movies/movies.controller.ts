6d951b76b2d8d149050d19c0378c3bc42eb84868
Izaias Neto <izaiasneto.dev@gmail.com>
2022-11-20 21:00:55 -0300

    Guard for all movies methods.

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,3 +1,4 @@
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { MoviesService } from './movies.service';
 import {
   Body,
@@ -7,6 +8,7 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { Movie } from './movie.entity';
 import { CreateMovieDto } from './dtos/create-movie.dto';
@@ -17,16 +19,19 @@ export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   async getAll(): Promise<Movie[]> {
     return await this.moviesService.getAll();
   }
 
   @Get('/:id')
+  @UseGuards(JwtAuthGuard)
   async getOne(@Param() id: string): Promise<Movie> {
     return await this.moviesService.getOne(id);
   }
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   async create(
     @Body() createMovieDto: CreateMovieDto,
   ): Promise<Partial<Movie>> {
@@ -34,11 +39,13 @@ export class MoviesController {
   }
 
   @Put('/:id')
+  @UseGuards(JwtAuthGuard)
   async update(@Body() updateMovieDto: UpdateMovieDto): Promise<Movie> {
     return await this.moviesService.update(updateMovieDto);
   }
 
   @Delete('/:id')
+  @UseGuards(JwtAuthGuard)
   async remove(@Param() id: string): Promise<any> {
     return await this.moviesService.remove(id);
   }
