53095f2877433e228227000bed873a7a5a50445f
Izaias Neto <izaiasneto.dev@gmail.com>
2022-11-21 00:13:02 -0300

    Add Swagger documentation.

--- src/movies/dtos/update-movie.dto.ts
+++ src/movies/dtos/update-movie.dto.ts
@@ -1,25 +1,46 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsDateString, IsEnum, IsOptional, IsString } from 'class-validator';
 import { MovieGenreEnum } from '../movie.types';
 
 export class UpdateMovieDto {
   @IsString()
   @IsOptional()
+  @ApiPropertyOptional({
+    description: 'Movie Title',
+    example: 'The Goodfellas',
+  })
   title: string;
 
   @IsString()
   @IsOptional()
+  @ApiPropertyOptional({
+    description: 'Movie Director',
+    example: 'Martin Scorcese',
+  })
   director: string;
 
   @IsString({ each: true })
   @IsEnum(MovieGenreEnum, { each: true })
   @IsOptional()
+  @ApiPropertyOptional({
+    description: 'Movie genres',
+    example: ['Action'],
+  })
   genre: MovieGenreEnum[];
 
   @IsString({ each: true })
   @IsOptional()
+  @ApiPropertyOptional({
+    description: 'Movie actors',
+    example: ['Robert De Niro', 'Joe Pesci'],
+  })
   cast: string[];
 
   @IsDateString()
   @IsOptional()
+  @ApiPropertyOptional({
+    description: 'Movie release date',
+    example: '123456',
+  })
   releaseDate: Date;
 }
--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,12 +1,29 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString, ValidateIf } from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'User email',
+    example: 'bob@gmail.com',
+  })
   email: string;
 
+  @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'User password',
+    example: 'mysupersecretpassword',
+  })
   password: string;
 
+  @IsString()
   @IsNotEmpty()
+  @ValidateIf((o) => o.password === o.confirmPassword)
+  @ApiProperty({
+    description: 'User password confirmation',
+    example: 'mysupersecretpassword',
+  })
   passwordConfirmation: string;
 }
