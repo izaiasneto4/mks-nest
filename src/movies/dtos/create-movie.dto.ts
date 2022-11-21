51a01386cf997d6733712ad2e3874226d4507fa8
Izaias Neto <izaiasneto.dev@gmail.com>
2022-11-20 21:05:24 -0300

    Add create movie dto.

--- src/movies/dtos/create-movie.dto.ts
+++ src/movies/dtos/create-movie.dto.ts
@@ -1,7 +1,22 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsDateString, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { MovieGenreEnum } from '../movie.types';
 
 export class CreateMovieDto {
   @IsString()
   @IsNotEmpty()
-  id: string;
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  director: string;
+
+  @IsString({ each: true })
+  @IsEnum(MovieGenreEnum, { each: true })
+  genre: MovieGenreEnum[];
+
+  @IsString({ each: true })
+  cast: string[];
+
+  @IsDateString()
+  releaseDate: Date;
 }
